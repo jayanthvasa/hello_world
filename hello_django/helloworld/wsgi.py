6b1956f4798c1743c8f95c405baac3237df2a707
jayanthvasa <vasajayanth10@gmail.com>
2025-04-27 23:56:10 +0530

    Add files via upload

---
+++ hello_django/helloworld/wsgi.py
@@ -0,0 +1,10 @@
+"""
+WSGI config for helloworld project.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
+
+application = get_wsgi_application()
